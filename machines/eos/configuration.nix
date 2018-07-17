659bc17641591059772616e5e01c1e3941c4acdb
Ben Challenor <ben@challenor.org>
2018-07-17 17:39:44 +0000

    Create eos, a basic EC2 machine

---
+++ machines/eos/configuration.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    <nixpkgs/nixos/modules/profiles/headless.nix>
+    ../../modules/common.nix
+    ../../modules/ec2.nix
+  ];
+
+  networking.hostName = "eos";
+
+  fileSystems."/" =
+    { device = "/dev/xvda1";
+      fsType = "ext4";
+    };
+
+  nix.maxJobs = 1;
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  users.mutableUsers = false;
+  users.users.admin = {
+    uid = 1000;
+    group = "admin";
+    createHome = true;
+    home = "/home/admin";
+    useDefaultShell = true;
+    extraGroups = [ "wheel" ];
+    hashedPassword = "*";
+    openssh.authorizedKeys.keyFiles = [
+      ../../keys/cloud-dev.pub
+      ../../keys/tablet-blink.pub
+      ../../keys/phone-termux.pub
+    ];
+  };
+  users.groups.admin = {
+    gid = 1000;
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  system.nixos.stateVersion = "18.09";
+}
