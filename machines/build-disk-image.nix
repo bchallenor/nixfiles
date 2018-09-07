8feae26cc897ef16ba9ff0ab93057ca1e9e30a55
Ben Challenor <ben@challenor.org>
2018-09-07 20:53:01 +0100

    Reduce nesting in build-disk-image.nix

--- machines/build-disk-image.nix
+++ machines/build-disk-image.nix
@@ -1,43 +1,44 @@
 {
   machineName,
-  machineSize
+  machineSize,
+  nixpkgs ? <nixpkgs>
 }:
-let
-  nixos = import <nixpkgs/nixos> {
-    configuration = { config, lib, pkgs, ... }:
-    let
-      machineConfigDir = pkgs.stdenv.mkDerivation {
-        name = "${machineName}-config-dir";
-        src = fetchGit {
-          url = ../.;
-        };
-        buildPhase = ''
-          ln -s machines/${machineName}/configuration.nix configuration.nix
-        '';
-        installPhase = ''
-          cp -R . $out
-        '';
-      };
 
-      machineConfig = (import <nixpkgs/nixos> {
-        configuration = import (machineConfigDir + /configuration.nix);
-      }).config;
-    in
-    {
-      config.system.build.diskImage = import <nixpkgs/nixos/lib/make-disk-image.nix> {
-        inherit lib pkgs;
-        name = "${machineName}-${machineConfigDir.src.shortRev}-disk-image";
-        config = machineConfig;
-        contents = [
-          # rsync needs trailing slash
-          { source = "${machineConfigDir}/"; target = "/etc/nixos"; }
-        ];
-        diskSize = machineSize;
-        partitionTableType = "legacy";
-        fsType = "ext4";
-        format = "raw";
-      };
+let
+  pkgs = import nixpkgs {};
+  lib = import (nixpkgs + /lib);
+  mkNixos = import (nixpkgs + /nixos);
+  mkDiskImage = import (nixpkgs + /nixos/lib/make-disk-image.nix);
+
+  machineConfigDir = pkgs.stdenv.mkDerivation {
+    name = "${machineName}-config-dir";
+    src = fetchGit {
+      url = ../.;
     };
+    buildPhase = ''
+      ln -s machines/${machineName}/configuration.nix configuration.nix
+    '';
+    installPhase = ''
+      cp -R . $out
+    '';
+  };
+
+  machineConfig = (mkNixos {
+    configuration = import (machineConfigDir + /configuration.nix);
+  }).config;
+
+  machineDiskImage = mkDiskImage {
+    inherit lib pkgs;
+    name = "${machineName}-${machineConfigDir.src.shortRev}-disk-image";
+    config = machineConfig;
+    contents = [
+      # rsync needs trailing slash
+      { source = "${machineConfigDir}/"; target = "/etc/nixos"; }
+    ];
+    diskSize = machineSize;
+    partitionTableType = "legacy";
+    fsType = "ext4";
+    format = "raw";
   };
 in
-  nixos.config.system.build.diskImage
+  machineDiskImage
