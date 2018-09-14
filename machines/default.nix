0953aa256f07cb191344ce4072adda78ef965261
Ben Challenor <ben@challenor.org>
2018-09-15 00:00:04 +0100

    Remove eos and nyx machines

--- machines/default.nix
+++ machines/default.nix
@@ -5,21 +5,9 @@ let
   buildMachineImage = import ./build-machine-image.nix;
 
 in {
-  eos = buildMachineImage {
-    inherit nixpkgs;
-    machineName = "eos";
-    machineSize = 2048;
-  };
-
   helios = buildMachineImage {
     inherit nixpkgs;
     machineName = "helios";
-    machineSize = 2048;
-  };
-
-  nyx = buildMachineImage {
-    inherit nixpkgs;
-    machineName = "nyx";
     machineSize = 2048;
   };
 
